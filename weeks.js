a57b4535621dd200d1acc4deda52a524b7535b46
Ármin Scipiades <scipiades.armin@gmail.com>
2019-07-16 20:42:37 +0200

    Add 'my life in weeks' visualisation

---
+++ weeks.js
@@ -0,0 +1,14 @@
+var events = {
+	'2009-05-23': {
+		'type': 'life-event',
+		'description': 'Összejöttünk Zsófival'
+	},
+	'2014-07-20': {
+		'type': 'life-event',
+		'description': "Munkába állok az E-Groupban"
+	},
+	'2019-02-23': {
+		'type': 'life-event',
+		'description': 'Szakítás Zsófival, költözés a Hajadon utcába'
+	}
+};
