318b3d58f7ea51da09af73b0dbb5e13aa393fc9a
Ármin Scipiades <scipiades.armin@gmail.com>
2019-07-16 21:38:20 +0200

    Add moving to Fényes Adolf utca life event

--- weeks.js
+++ weeks.js
@@ -3,6 +3,10 @@ var events = {
 		'type': 'life-event',
 		'description': 'Összejöttünk Zsófival'
 	},
+	'2013-05-31': {
+		'type': 'life-event',
+		'description': 'Költözés a Fényes Adolf utcába'
+	}
 	'2014-07-20': {
 		'type': 'life-event',
 		'description': "Munkába állok az E-Groupban"
